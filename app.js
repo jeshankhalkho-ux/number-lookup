e10d3729f5522a9d9f03dc330cce68274a97d294
jeshankhalkho-ux <jeshankhalkho@gmail.com>
2026-03-29 20:01:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-import express from "express";
-import cors from "cors";
-import router from "./routes/index.js";
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use("/api", router);
-
-export default app;
